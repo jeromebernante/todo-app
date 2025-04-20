f4bb11695909abd1bfd858e41ed0cd8dc39175da
Jerome <jeromebernante@gmail.com>
2025-04-20 22:33:07 +0800

    redesign : inputs, icons, confirmation message

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,5 @@
 import {  useState } from 'react';
+import { PlusIcon } from '@heroicons/react/24/outline';
 
 interface TodoFormProps {
     addTodo: (text: string) => void;
@@ -16,19 +17,19 @@ const TodoForm: React.FC<TodoFormProps> = ({ addTodo }) => {
     }
 
     return (
-        <div className='mb-4'>
+        <form className='mb-4 flex relative'>
             <input 
                 type="text" 
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
                 placeholder='Add a new todo'
-                className='w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+                className='w-full p-2 pe-[55px] border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
             />
             <button
                 onClick={handleSubmit}
-                className='mt-2 w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600'
-            >Add To Do</button>
-        </div>
+                className='absolute top-[2px] right-[2px] bottom-[2px] bg-blue-500 w-11 text-white rounded-md hover:bg-blue-600 cursor-pointer flex items-center justify-center'
+            ><PlusIcon className='h-6 w-6'/></button>
+        </form>
     );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import { Todo } from '../types/Todo';
 import { useState } from 'react';
+import { PencilIcon, TrashIcon, CheckIcon } from '@heroicons/react/24/solid';
 
 interface TodoListProps {
     todos: Todo[];
@@ -35,27 +36,32 @@ const TodoList: React.FC<TodoListProps> = ({
             {[...todos].reverse().map((todo) => (
                 <li
                     key={todo.id}
-                    className='flex items-center justify-between p-2 bg-gray-50 rounded-md'
+                    className='flex items-center justify-between bg-gray-50 rounded-md'
                 >
                     {editingId === todo.id ? (
-                        <div className='flex-1 flex items-center space-x-2'>
+                        <div className='relative flex-1 flex items-center'>
                             <input
                                 type="text"
                                 value={editText}
                                 onChange={(e) => setEditText(e.target.value)}
-                                className='flex-1 p-1 border rounded-md'
+                                className='flex-1 p-2 pe-[55px] border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
                             />
                             <button
                                 onClick={() => handleSave(todo.id)}
-                                className='bg-gree-500 text-white px-2 py-1 rounded-md bg-green-500 hover:bg-green-600'
-                            >Save</button>
+                                className='absolute flex items-center justify-center cursor-pointer w-11 top-[2px] right-[2px] bottom-[2px] bg-gree-500 text-white px-2 py-1 rounded-md bg-green-500 hover:bg-green-600'
+                            > <CheckIcon className='h-4 w-4' /></button>
                         </div>
                         ) : (
-                            <div className='flex-1 flex items-center space-x-2'>
+                            <div className='flex-1 flex items-center space-x-2 p-2 border border-white'>
                                 <input 
                                     type='checkbox'
                                     checked={todo.completed}
-                                    onChange={() => toggleTodo(todo.id)}
+                                    onChange={() => {
+                                        const confirmToggle = window.confirm(`Are sure you want to mark this task as ${todo.completed ? 'incomplete' : 'completed'}?`);
+                                        if (confirmToggle) {
+                                            toggleTodo(todo.id);
+                                        }
+                                    }}
                                     className='h-4 w-4'
                                 />
                                 <span
@@ -65,12 +71,17 @@ const TodoList: React.FC<TodoListProps> = ({
                                 >{todo.text}</span>
                                 <button
                                     onClick={() => handleEdit(todo)}
-                                    className='text-blue-500 hover:text-blue-700'
-                                >Edit</button>
+                                    className='me-3 text-blue-500 hover:text-blue-700 cursor-pointer'
+                                ><PencilIcon className='h-6 w-6' /></button>
                                 <button
-                                    onClick={() => deleteTodo(todo.id)}
-                                    className='text-red-500 hover:text-red-700'
-                                >Delete</button>
+                                    onClick={() => {
+                                        const confirmDelete = window.confirm('Are you sure you want to delete this task?');
+                                        if (confirmDelete) {
+                                            deleteTodo(todo.id);
+                                        }
+                                    }}
+                                    className='text-red-500 hover:text-red-700 cursor-pointer'
+                                ><TrashIcon className='h-6 w-6' /></button>
                             </div>    
                     )}
                 </li>
